bb9e8812eed4f6497b4bc65469d62b4db5af981a
Saeed Abdulrauf <abdulraufsaeed923@gmail.com>
2024-07-22 23:02:56 -0700

    Created a create_group_page and profile_edit page

--- frontend/script.js
+++ frontend/script.js
@@ -20,3 +20,15 @@ function hideNavigationIfVisible() {
       navigations.style.top = '-224px';
   }
 }
+
+
+let plus_navigations_show = document.getElementById('plus_navigations_show');
+let plus_navigations = document.getElementById('plus_navigations');
+
+plus_navigations_show.addEventListener('click', () => {
+  if (plus_navigations.style.display == 'none') {
+    plus_navigations.style.display = 'block'
+  } else {
+    plus_navigations.style.display = 'none'
+  }
+})
